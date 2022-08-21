0c5fff0dcfc281dbbe39aa146fa1cb2a0cbaf563
Henrik Sundman <henriksundman@yahoo.se>
2022-08-21 15:26:12 +0200

    fix card sizes fix statusbar

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Platform } from 'react-native';
 
 import { MainContainer } from './screens/MainContainer';
 import { GameContextProvider } from './store/game-context';
 
 export default function App() {
 	return (
-		<SafeAreaView style={{ flex: 1 }}>
+		<SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
 			<GameContextProvider>
 				<View style={styles.container}>
-					<StatusBar style="auto" />
+					<StatusBar style={Platform.OS === 'ios' ? 'light' : 'dark'} />
 					<MainContainer />
 				</View>
 			</GameContextProvider>
@@ -20,7 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#45ed54',
+		backgroundColor: '#bbdff5',
 		alignItems: 'center',
+		justifyContent: 'center',
 	},
 });
--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,24 +1,28 @@
-import { StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, useWindowDimensions, View } from 'react-native';
 
 interface MainScreenProps {
 	children: React.ReactNode;
 }
 
 export const MainScreen = ({ children }: MainScreenProps) => {
+	const cardWidth = useWindowDimensions().width * 0.9;
+	const cardHeightIOS = useWindowDimensions().height * 0.8;
+	const cardHeightAndroid = useWindowDimensions().height * 0.9;
+
+	const styles = StyleSheet.create({
+		card: {
+			marginTop: 20,
+			alignItems: 'center',
+			height: Platform.OS === 'ios' ? cardHeightIOS : cardHeightAndroid,
+			width: cardWidth,
+			elevation: 4,
+			backgroundColor: 'white',
+			shadowColor: '#171717',
+			shadowOffset: { width: -2, height: 4 },
+			shadowOpacity: 0.2,
+			shadowRadius: 3,
+			borderRadius: 6,
+		},
+	});
 	return <View style={styles.card}>{children}</View>;
 };
-
-const styles = StyleSheet.create({
-	card: {
-		marginTop: 20,
-		alignItems: 'center',
-		height: '90%',
-		elevation: 4,
-		backgroundColor: 'white',
-		shadowColor: '#171717',
-		shadowOffset: { width: -2, height: 4 },
-		shadowOpacity: 0.2,
-		shadowRadius: 3,
-		borderRadius: 6,
-	},
-});
