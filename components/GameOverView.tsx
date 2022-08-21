7f96e543be73e3784fbb4fa49a3785d1b297a5d2
Henrik Sundman <henriksundman@yahoo.se>
2022-08-21 20:30:54 +0200

    add game over view

--- components/GameOverView.tsx
+++ components/GameOverView.tsx
@@ -1,11 +1,28 @@
-import { Text, View } from 'react-native';
+import { useContext } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 import { GameContext } from '../store/game-context';
 
 export const GameOverView = () => {
+	const {
+		numberOfQuestions,
+		numberOfCorrectAnswers,
+		numberOfIncorrectAnswers,
+	} = useContext(GameContext);
+
 	return (
-		<View>
-			<Text>GameOverView</Text>
+		<View style={styles.container}>
+			<Text>
+				You answered {numberOfCorrectAnswers} out of {numberOfQuestions}{' '}
+				{numberOfQuestions > 1 ? 'questions' : 'question'} correctly
+			</Text>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+	},
+});
